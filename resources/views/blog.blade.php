45011d19b3321d81bc246e4a53529e1c6ce3f596
akhdanfarrosrenoldi <renoldiakhdanfarros@gmail.com>
2025-05-24 14:34:24 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul satu</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora architecto quia voluptatem culpa ab
+            aspernatur adipisci voluptatibus fugiat ut dolorum? Quos illo eaque adipisci, magni autem accusantium vel ea
+            reprehenderit?</p>
+    </article>
+    <article>
+        <h3>Judul Dua</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia error soluta commodi fuga quibusdam magni
+            minus quisquam earum, quaerat consectetur harum a officia illo sed reprehenderit vel culpa fugiat officiis.
+        </p>
+    </article>
+</body>
+
+</html>
